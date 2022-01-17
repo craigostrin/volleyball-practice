145e3dda17407921a16af46ea216c7347b5e52b4
craig <craigostrin@gmail.com>
2022-01-17 00:33:17 -0800

    continued work on input remapping (WIP)

--- src/UI/ControlsMenu.gd
+++ src/UI/ControlsMenu.gd
@@ -1,3 +1,20 @@
 extends Control
 
-onready var _action_list
+onready var _input_mapper: Node = $InputMapper
+onready var _action_list: VBoxContainer = $VBox/ScrollContainer/ActionList
+onready var _profile_selector: OptionButton = $VBox/ProfileSelector
+
+func _ready() -> void:
+	_input_mapper.connect("profile_changed", self, "rebuild")
+	_profile_selector.initialize(_input_mapper)
+	_input_mapper.change_profile(_profile_selector.selected)
+
+
+func rebuild(input_profile, is_customizable := false):
+	_action_list.clear()
+	for input_action in input_profile.keys():
+		var line = _action_list.add_input_line(input_action, \
+			input_profile[input_action], is_customizable)
+		if is_customizable:
+			line.connect("change_button_pressed", self, \
+				"_on_Input_line_change_button_pressed", [input_action, line])
